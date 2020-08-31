7e8f126cde47c09930016cbdb292a3eb6d21911f
Porfirio <porfirioperezgon@gmail.com>
2020-08-30 22:15:16 -0400

    clean user dto

--- GeJu.Back/GeJu.Api.Main/DTO/Users/CreateUserDTO.cs
+++ GeJu.Back/GeJu.Api.Main/DTO/Users/CreateUserDTO.cs
@@ -1,9 +1,6 @@
 ﻿using GeJu.Common;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GeJu.Api.Main.DTO.Users
 {
