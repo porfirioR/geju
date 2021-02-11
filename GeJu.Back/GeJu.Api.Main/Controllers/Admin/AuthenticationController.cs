ee5ef36a0ba9c382dd81743095e24f5def53e1e2
Porfirio Perez <porfirioperezgon@gmail.com>
2021-02-11 17:55:59 -0300

    authcontroller

---
+++ GeJu.Back/GeJu.Api.Main/Controllers/Admin/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿using GeJu.Sql;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeJu.Api.Main.Controllers.Admin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public AuthenticationController(DataContext context)
+        {
+            _dataContext = context;
+        }
+    }
+}
