5f69f73aed8cc4aa85fe85736fb95e87077ff305
porfirioR <porfirioperezgon@gmail.com>
2021-03-28 00:12:24 -0400

    login

--- geju-SPA/src/app/core/components/login/login.component.ts
+++ geju-SPA/src/app/core/components/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { LoginRequest } from '../../models/api/login-request';
 import { AccountService } from '../../services/api/account.service';
@@ -8,26 +9,38 @@ import { AccountService } from '../../services/api/account.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   public loginForm: FormGroup;
   public loginInvalid: boolean;
+  private subscriptions: Subscription[] = [];
 
-  constructor(private readonly accountService: AccountService, private readonly router: Router) { }
+  constructor(
+    private readonly accountService: AccountService,
+    private readonly router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.accountService.currentUser$.pipe(
-      map(user => {
-        if (user) {
-          this.router.navigate(['administracion']);
-        }
-      })
+    this.subscriptions.push(
+      this.accountService.currentUser$
+        .pipe(
+          map((user) => {
+            if (user) {
+              this.router.navigate(['administracion']);
+            }
+          })
+        )
+        .subscribe()
     );
     this.loginForm = new FormGroup({
       email: new FormControl('', Validators.email),
-      password: new FormControl('', Validators.required)
+      password: new FormControl('', Validators.required),
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((x) => x.unsubscribe());
   }
 
   public onSubmit = (): void => {
@@ -35,12 +48,16 @@ export class LoginComponent implements OnInit {
     if (this.loginForm.valid) {
       const loginRequest: LoginRequest = this.loginForm.value;
       this.accountService.login(loginRequest).pipe(
-        map(x => this.router.navigate(['administracion'])),
-        catchError(err => {
+        map((x) => this.router.navigate(['administracion'])),
+        catchError((err) => {
           this.loginInvalid = true;
           return err.error;
         })
       ).subscribe();
     }
+  }
+
+  public close = () => {
+    this.router.navigate(['']);
   }
 }
